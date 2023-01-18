a437dad19a98beae6f715e8c3febc1c5c9ed46fd
Marcio Reis <dev.marcioreis@gmail.com>
2023-01-18 09:05:42 -0300

    add project

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+gsap.from('#text', {
+    scrollTrigger : {
+        scrub: true
+    },
+    x: 650, 
+})
+
+gsap.from('#clouds', {
+    scrollTrigger : {
+        scrub: true
+    },
+    x: -700, 
+})
+
+gsap.from('#moon', {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 700, 
+})
+
+gsap.from('#man', {
+    scrollTrigger : {
+        scrub: true
+    },
+    x: -250, 
+})
+
+gsap.from('#plants', {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 120, 
+})
+
+gsap.from('#trees', {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 120, 
+})
+
+
